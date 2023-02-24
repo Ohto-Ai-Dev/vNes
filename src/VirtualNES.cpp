1307021e735dcf2021479ea705a27ceb82004417
Ohto-Ai <zhu.thatboy@outlook.com>
2023-02-24 20:58:32 +0800

    Show website option in menu

--- src/VirtualNES.cpp
+++ src/VirtualNES.cpp
@@ -1,9 +1,16 @@
 #include "VirtualNES.h"
+#include <QDesktopServices>
+#include <QUrl>
 
 VirtualNES::VirtualNES(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+
+    connect(ui.actionWebsite, &QAction::triggered, [] {
+        QDesktopServices::openUrl(QUrl{ "https://ohto-ai-dev.github.io/vNes/" });
+        });
+
 }
 
 VirtualNES::~VirtualNES()
