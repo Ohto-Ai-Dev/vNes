1a5eaad739b9538f4c9af868bae8b23c7e53b6ff
请问墙和柱子能奶你吗 <ohtoai.choo@gmail.com>
2023-03-08 23:23:07 +0800

    Add NesRom (#21)
    - Add rom load demo

--- src/VirtualNES.cpp
+++ src/VirtualNES.cpp
@@ -1,6 +1,9 @@
-#include "VirtualNES.h"
 #include <QDesktopServices>
 #include <QUrl>
+#include <QFileDialog>
+#include <QMessageBox>
+#include "VirtualNES.h"
+#include "nes/NesRom.hpp"
 
 VirtualNES::VirtualNES(QWidget *parent)
     : QMainWindow(parent)
@@ -11,6 +14,20 @@ VirtualNES::VirtualNES(QWidget *parent)
         QDesktopServices::openUrl(QUrl{ "https://ohto-ai-dev.github.io/vNes/" });
         });
 
+    connect(ui.actionLoadNesRom, &QAction::triggered, [this] {
+        QString path = QFileDialog::getOpenFileName(this, "Open NES Rom", "", "NES Rom (*.nes)");
+        NesRom rom;
+        try {
+            rom.load(path.toStdString());
+        }
+        catch (const std::exception& e) {
+            QMessageBox::critical(this, "Error", e.what());
+            return;
+        }
+        const auto& prg_rom = rom.prg_rom();
+        const auto& chr_rom = rom.chr_rom();
+        QMessageBox::information(this, "Tip", QString::asprintf("PRG ROM size: %d bytes, CHR ROM size: %d bytes", prg_rom.size(), chr_rom.size()));
+        });
 }
 
 VirtualNES::~VirtualNES()
